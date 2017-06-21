76ae6d90cd366d22d708369f67310120b7fb35c2
do.korablev <krablevd@ya.ru>
2017-06-21 17:51:30 +0300

    null-д\з 2.0.1

--- src/main/java/com/acme/edu/App.java
+++ src/main/java/com/acme/edu/App.java
@@ -6,6 +6,14 @@ package com.acme.edu;
  */
 public class App {
     public static void main( String[] args ) {
+
         System.out.println( "Hello World!" );
+
+        Hi("HW-2.0");
+    }
+
+    private static void Hi(String measseg){
+
+        System.out.println(measseg);
     }
 }
