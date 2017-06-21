d8cde2204d10e46d5dfa4ed606613cac17f69bcd
do.korablev <krablevd@ya.ru>
2017-06-21 17:29:50 +0300

    Fail

--- src/main/java/com/acme/edu/App.java
+++ src/main/java/com/acme/edu/App.java
@@ -6,13 +6,8 @@ package com.acme.edu;
  */
 public class App 
 {
-    public static void main( String[] args ){
+    public static void main( String[] args )
+    {
         System.out.println( "Hello World!" );
-
-        Hi("2.0");
-    }
-
-    private static void Hi(String meassage){
-        System.out.println(meassage);
     }
 }
