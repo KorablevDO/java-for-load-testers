fa857be05dae962d59984197abcabd6cb88d4ca4
do.korablev <krablevd@ya.ru>
2017-06-30 10:30:24 +0300

    Рефакторинг калькулятора

--- src/main/java/com/acme/edu/Сalculator.java
+++ src/main/java/com/acme/edu/Сalculator.java
@@ -4,42 +4,83 @@ package com.acme.edu;
  * Created by do.korablev on 26.06.2017.
  */
 public class Сalculator {
-    private static int memori = 0;
-    private static int operationMemori = 0;
+    private static double storedNumber = 0;
+    private static double lastNumberObtained = 0;
 
     public static int summa(int a, int b) {
-        operationMemori = a + b;
-        return operationMemori;
+        lastNumberObtained = checkForRestrictions( a ) + checkForRestrictions( b );
+        return (int)checkForRestrictions( lastNumberObtained );
     }
 
     public static int subtraction(int a, int b) {
-        operationMemori = a - b;
-        return operationMemori;
+        lastNumberObtained = checkForRestrictions( a ) - checkForRestrictions( b );
+        return (int)checkForRestrictions( lastNumberObtained );
     }
 
     public static int multiplication(int a, int b) {
-        operationMemori = a * b;
-        return operationMemori;
+        lastNumberObtained = checkForRestrictions( a ) * checkForRestrictions( b );
+        return (int)checkForRestrictions( lastNumberObtained );
     }
 
     public static int division(int a, int b) {
-        operationMemori = a / b;
-        return operationMemori;
+        lastNumberObtained = checkForRestrictions( a ) / checkForRestrictions( b );
+        return (int)checkForRestrictions( lastNumberObtained );
     }
 
-    public static void write(int a) {
-        operationMemori = a;
+    public static double summa(double a, double b) {
+        lastNumberObtained = checkForRestrictions( a ) + checkForRestrictions( b );
+        return checkForRestrictions( lastNumberObtained );
+    }
+
+    public static double subtraction(double a, double b) {
+        lastNumberObtained = checkForRestrictions( a ) - checkForRestrictions( b );
+        return checkForRestrictions( lastNumberObtained );
+    }
+
+    public static double multiplication(double a, double b) {
+        lastNumberObtained = checkForRestrictions( a ) * checkForRestrictions( b );
+        return checkForRestrictions( lastNumberObtained );
+    }
+
+    public static double division(double a, double b) {
+        lastNumberObtained = checkForRestrictions( a ) / checkForRestrictions( b );
+        return checkForRestrictions( lastNumberObtained );
+    }
+
+    public static void setMemory(double a) {
+        lastNumberObtained = checkForRestrictions( a );
     }
 
     public static void memory() {
-        memori = operationMemori;
+        storedNumber = lastNumberObtained;
     }
 
-    public static int memoryPlus() {
-        return memori + operationMemori;
+    public static double getMemoryPlus() {
+        return checkForRestrictions(storedNumber + lastNumberObtained);
     }
 
-    public static int memoryMinus() {
-        return memori - operationMemori;
+
+    public static double getMemoryMinus() {
+        return checkForRestrictions(storedNumber - lastNumberObtained);
+    }
+
+    public static double getMemory(){
+        return storedNumber;
+    }
+
+    private static double checkForRestrictions(double number){
+        int limit = 10;
+
+        if(number > limit){
+            return limit;
+        }else if(number < -limit){
+            return -limit;
+        }else {
+            return number;
+        }
+    }
+
+    public static double module(double number){
+       return number * -1;
     }
 }
--- src/test/java/com/acme/edu/TestCalculator.java
+++ src/test/java/com/acme/edu/TestCalculator.java
@@ -6,14 +6,180 @@ package com.acme.edu;
 
 public class TestCalculator {
     public static void main(String[] args) {
-        System.out.println(Сalculator.summa(10, 10));
-        Сalculator.write(6);
+        summa();
+        subtraction();
+        setMemoryAndMemoryAndGetMemory();
+        multiplication();
+        division();
+        memoryPlus();
+        memoryMinus();
+        module();
+    }
+
+    private static void summa(){
+        int a = 4;
+        int b = 4;
+        double c = 3.2;
+        double d = 1.9;
+
+        int result1 = Сalculator.summa(a, b);
+        double result2 = Сalculator.summa(d, c);
+
+        if((result1 > a) && (result1 > b)){
+            System.out.println("summa pass " + result1);
+        }else{
+            System.out.println("summa Fail " + result1);
+        }
+
+        if((result2 > c) && (result2 > d)){
+            System.out.println("summa pass " + result2);
+        }else{
+            System.out.println("summa Fail " + result2);
+        }
+
+        Сalculator.setMemory(0);
         Сalculator.memory();
-        System.out.println(Сalculator.subtraction(10, 5));
-        System.out.println(Сalculator.memoryPlus());
-        System.out.println(Сalculator.multiplication(10, 10));
+    }
+
+    private static void subtraction(){
+        int a = 4;
+        int b = 4;
+        double c = 3.2;
+        double d = 1.9;
+
+        int result1 = Сalculator.subtraction(a, b);
+        double result2 = Сalculator.subtraction(d, c);
+
+        if((result1 < a) && (result1 < b)){
+            System.out.println("subtraction pass " + result1);
+        }else{
+            System.out.println("subtraction Fail " + result1);
+        }
+
+        if((result2 < c) && (result2 < d)){
+            System.out.println("subtraction pass " + result2);
+        }else{
+            System.out.println("subtraction Fail " + result2);
+        }
+
+        Сalculator.setMemory(0);
         Сalculator.memory();
-        System.out.println(Сalculator.division(50, 5));
-        System.out.println(Сalculator.memoryMinus());
+    }
+
+    private static void setMemoryAndMemoryAndGetMemory(){
+        int a = 6;
+        double b = 3.3;
+
+        Сalculator.setMemory(a);
+        Сalculator.memory();
+        System.out.println(a + " -> " + Сalculator.getMemory());
+
+        Сalculator.setMemory(b);
+        Сalculator.memory();
+        System.out.println(b + " -> " + Сalculator.getMemory());
+
+        Сalculator.setMemory(0);
+        Сalculator.memory();
+    }
+
+    private static void multiplication(){
+        int a = 2;
+        int b = 2;
+        double c = 3.2;
+        double d = 1.9;
+
+        int result1 = Сalculator.multiplication(a, b);
+        double result2 = Сalculator.multiplication(d, c);
+
+        if((result1 > a) && (result1 > b)){
+            System.out.println("multiplication pass " + result1);
+        }else{
+            System.out.println("multiplication Fail " + result1);
+        }
+
+        if((result2 > c) && (result2 > d)){
+            System.out.println("multiplication pass " + result2);
+        }else{
+            System.out.println("multiplication Fail " + result2);
+        }
+
+        Сalculator.setMemory(0);
+        Сalculator.memory();
+    }
+
+    private static void division(){
+        int a = 8;
+        int b = 4;
+        double c = 4.2;
+        double d = 1.9;
+
+        int result1 = Сalculator.division(a, b);
+        double result2 = Сalculator.division(d, c);
+
+        if((result1 < a) && (result1 < b)){
+            System.out.println("division pass " + result1);
+        }else{
+            System.out.println("division Fail " + result1);
+        }
+
+        if((result2 < c) && (result2 < d)){
+            System.out.println("division pass " + result2);
+        }else{
+            System.out.println("division Fail " + result2);
+        }
+
+        Сalculator.setMemory(0);
+        Сalculator.memory();
+    }
+
+    private static void memoryPlus(){
+        int a = 2;
+        int b = 1;
+        double c = 3.3;
+        double d = 2.2;
+
+        Сalculator.setMemory(a);
+        Сalculator.memory();
+        System.out.println("memoryPlus " + a + "+" + Сalculator.summa(a, b) + " = " + Сalculator.getMemoryPlus());
+
+        Сalculator.setMemory(0);
+        Сalculator.memory();
+
+        Сalculator.setMemory(b);
+        Сalculator.memory();
+        Сalculator.summa(c, d);
+        System.out.println("memoryPlus " + b + "+" + Сalculator.summa(c, b) + " = " + Сalculator.getMemoryPlus());
+
+        Сalculator.setMemory(0);
+        Сalculator.memory();
+    }
+
+    private static void memoryMinus() {
+        int a = 8;
+        int b = 2;
+        double c = 8.3;
+        double d = 2.2;
+
+        Сalculator.setMemory(a);
+        Сalculator.memory();
+        System.out.println("memoryMinus " + a + "-" + Сalculator.subtraction(a, b) + " = " + Сalculator.getMemoryMinus());
+
+        Сalculator.setMemory(0);
+        Сalculator.memory();
+
+        Сalculator.setMemory(c);
+        Сalculator.memory();
+        Сalculator.summa(c, d);
+        System.out.println("memoryMinus " + b + "-" + Сalculator.subtraction(c, b) + " = " + Сalculator.getMemoryMinus());
+
+        Сalculator.setMemory(0);
+        Сalculator.memory();
+    }
+
+    public static void module() {
+        int a = -5;
+        double b = -2.1;
+        System.out.println("module " + a + " -> " + Сalculator.module(a));
+        System.out.println("module " + b + " -> " + Сalculator.module(b));
     }
 }
